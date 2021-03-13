9bba888a08cb4fd007a6398d2dddfd2b7ce1d29d
pinakiganguly <59434679+pinakiganguly@users.noreply.github.com>
2021-03-14 00:30:11 +0530

    Add files via upload

---
+++ date_time_module/time_demo.py
@@ -0,0 +1,17 @@
+import time
+#print(time.time())
+#-----------------------------------------------
+#print(time.localtime())
+#-----------------------------------------------
+from datetime import date
+#print(date.today())
+#print(date.today().month)
+#print(date.today().day)
+import datetime
+#print(datetime.date(2021,2,20))
+
+#x=datetime.date(2021,2,20)
+#print(x.day)
+#-----------------------------------------------
+print(date.fromtimestamp(1614101032))
+
